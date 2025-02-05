2680fdee7ba1bc3f4c04f86ac79c5eb1bc6dd8aa
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-02-05 17:14:51 +0000

    Navbar buttons centered

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
 
   return (
-    <div className="w-full bg-gray-200 px-4 py-2 fixed top-0 left-0 h-20 flex justify-center gap-20 cursor-pointer shadow-md z-50">
+    <div className="w-full bg-gray-200 px-4 py-2 fixed top-0 left-0 h-20 flex justify-center gap-20 cursor-pointer shadow-md z-50 items-center font-medium">
       <Link to="/" className="relative px-4 py-2 h-10 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-gray-700 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">Home</Link>
       <Link to="/products" className="relative px-4 py-2 h-10 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-gray-700 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">Products</Link>
       <Link to="/checkout" className="relative px-4 py-2 h-10 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-gray-700 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">Checkout</Link>
